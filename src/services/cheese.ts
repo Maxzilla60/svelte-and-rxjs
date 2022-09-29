54bbe30f6a7eb13660dc9c4004b37dc33cc7f4bd
Maxime Orione <maxime.orione@cegeka.com>
2022-09-29 13:32:14 +0200

    🔥 Delete services

--- src/services/cheese.ts
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const cheeseCount$ = writable<number>(1);
-
-/*
-cheeseCount$.set(0);
-cheeseCount$.update(prev => prev + 1);
-cheeseCount$.subscribe(newValue => console.log(newValue));
-*/
