41abab305dd542ad2eb2848c7ab33fa1fd2c151c
Maxime Orione <maxime.orione@cegeka.com>
2022-09-29 15:54:46 +0200

    ✨ Create RxJS version of ValueFilteringDemo component

---
+++ src/components/ValueFilteringRxJSDemo.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { Fruit, fruit$ } from '../services/fruitObservable';
+	import BouncyFruit from './BouncyFruit.svelte';
+	import { filter, Observable } from 'rxjs';
+
+	const bananaCount$: Observable<Fruit> = fruit$.pipe(
+		filter(event => event?.fruit === 'banana'),
+	);
+	const kiwiCount$: Observable<Fruit> = fruit$.pipe(
+		filter(event => event?.fruit === 'kiwi'),
+	);
+	const tomatoCount$: Observable<Fruit> = fruit$.pipe(
+		filter(event => event?.fruit === 'tomato'),
+	);
+</script>
+
+<div id="bouncy_fruits">
+	<BouncyFruit fruitEmoji="🍌" fruitCount$={bananaCount$}/>
+	<BouncyFruit fruitEmoji="🥝" fruitCount$={kiwiCount$}/>
+	<BouncyFruit fruitEmoji="🍅" fruitCount$={tomatoCount$}/>
+</div>
+
+<style>
+	#bouncy_fruits {
+		display: inline-flex;
+	}
+</style>
