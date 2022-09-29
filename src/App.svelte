428048f3ad23919e53841aef4a992e9207524abe
Maxime Orione <maxime.orione@cegeka.com>
2022-09-29 15:31:42 +0200

    🚚 Move value filtering demo to its own component

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,6 @@
 <script lang="ts">
-	import { Writable, writable } from 'svelte/store';
-	import { fruit$ } from './services/fruitStore';
-	import BouncyFruit from './components/BouncyFruit.svelte';
 	import SubscribeAndDerivedDemo from './components/SubscribeAndDerivedDemo.svelte';
-
-	const bananaCount$ = writable<number>(0);
-	const kiwiCount$ = writable<number>(0);
-	const tomatoCount$ = writable<number>(0);
-
-	createCountSubscription(bananaCount$, 'banana');
-	createCountSubscription(kiwiCount$, 'kiwi');
-	createCountSubscription(tomatoCount$, 'tomato');
-
-	function createCountSubscription(count$: Writable<number>, tomato: string): void {
-		fruit$.subscribe(fruit => {
-			if (fruit?.fruit === tomato) {
-				count$.update(c => c + 1);
-			}
-		});
-	}
+	import ValueFilteringDemo from './components/ValueFilteringDemo.svelte';
 </script>
 
 <main>
@@ -29,11 +11,7 @@
 
 	<p>---</p>
 
-	<div id="bouncy_fruits">
-		<BouncyFruit fruitEmoji="🍌" fruitCount$={bananaCount$}/>
-		<BouncyFruit fruitEmoji="🥝" fruitCount$={kiwiCount$}/>
-		<BouncyFruit fruitEmoji="🍅" fruitCount$={tomatoCount$}/>
-	</div>
+	<ValueFilteringDemo/>
 </main>
 
 <style>
@@ -61,9 +39,5 @@
 	h5 {
 		margin-top: 0;
 		font-weight: normal;
-	}
-
-	#bouncy_fruits {
-		display: inline-flex;
 	}
 </style>
---
+++ src/components/ValueFilteringDemo.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { Writable, writable } from 'svelte/store';
+	import { fruit$ } from '../services/fruitStore';
+	import BouncyFruit from './BouncyFruit.svelte';
+
+	const bananaCount$ = writable<number>(0);
+	const kiwiCount$ = writable<number>(0);
+	const tomatoCount$ = writable<number>(0);
+
+	createCountSubscription(bananaCount$, 'banana');
+	createCountSubscription(kiwiCount$, 'kiwi');
+	createCountSubscription(tomatoCount$, 'tomato');
+
+	function createCountSubscription(count$: Writable<number>, tomato: string): void {
+		fruit$.subscribe(fruit => {
+			if (fruit?.fruit === tomato) {
+				count$.update(c => c + 1);
+			}
+		});
+	}
+</script>
+
+<div id="bouncy_fruits">
+	<BouncyFruit fruitEmoji="🍌" fruitCount$={bananaCount$}/>
+	<BouncyFruit fruitEmoji="🥝" fruitCount$={kiwiCount$}/>
+	<BouncyFruit fruitEmoji="🍅" fruitCount$={tomatoCount$}/>
+</div>
+
+<style>
+	#bouncy_fruits {
+		display: inline-flex;
+	}
+</style>
